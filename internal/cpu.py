4b2c7741ee3b77e285ca128a6b06ae6ce621f0d8
Maria Volpe <mariaCHVolpe@gmail.com>
2018-05-10 19:00:00 -0400

    Fix unexpected process demotion when returning from I/O queue and fix updating pages in memory

--- internal/cpu.py
+++ internal/cpu.py
@@ -61,6 +61,7 @@ class CPU:
     def time_quantum(self):
         #do nothing if CPU is not being used
         if self.using_CPU == None:
+            print("Can't increase time quantum. CPU is idle.")
             return
 
         self.using_CPU.time_quantum +=1
@@ -68,6 +69,8 @@ class CPU:
             self.preempt()
         if self.using_CPU.level == 1 and self.using_CPU.time_quantum == 2:
             self.preempt()
+        if self.using_CPU.level == 2:
+            print("Increasing time quantum has no effect. Process in CPU belongs to level 2.")
 
     #preempt if process has exceeded time quantums allowed on for its level
     #add process to queue one level below its current priority level
@@ -78,7 +81,7 @@ class CPU:
         if self.using_CPU.level == 0:
             process.level = 1
             self.lvl_1_q.append(process)
-        else:
+        elif self.using_CPU.level == 1:
             process.level = 2
             self.lvl_2_q.append(process)
         self.using_CPU = None
@@ -97,6 +100,7 @@ class CPU:
     def terminate(self):
         #do nothing if CPU is not being used
         if self.using_CPU == None:
+            print("Can't terminate. CPU is idle.")
             return
 
         #reclaim memory
@@ -108,9 +112,11 @@ class CPU:
     def request_io(self, num, file_name):
         #do nothing if no process is using CPU
         if self.using_CPU == None:
+            print("Cannot request I/O. CPU is idle.")
             return
         #do nothing if disk requested does not exist
         elif int(num) >= self.disk_count:
+            print("Specified disk number does not exist.")
             return
 
         self.disks[int(num)].request_io(file_name, self.using_CPU)
@@ -122,18 +128,20 @@ class CPU:
     def terminate_io(self, num):
         #do nothing if disk requested does not exist
         if int(num) >= self.disk_count:
+            print("Specified disk number does not exist.")
             return
         #do nothing if disk requested is not being used by any process
         elif self.disks[int(num)].using_HDD == None:
+            print("Cannot terminate I/O usage for disk {}. Disk is idle.".format(num))
             return
-
+        
         process = self.disks[int(num)].terminate_io()
 
         #process returned from hdd.terminate_io is put back into ready-queue
-        if self.using_CPU.level == 0:
+        if process.level == 0:
             self.lvl_0_q.append(process)
-        elif self.using_CPU.level == 1:
-            lvl_1_q.append(process)
+        elif process.level == 1:
+            self.lvl_1_q.append(process)
         else:
             self.lvl_2_q.append(process)
         self.refresh_lvl_0()
@@ -143,6 +151,7 @@ class CPU:
     def access_memory(self, page):
         #do nothing if no process is using CPU
         if self.using_CPU == None:
+            print ("Cannot access memory for process using CPU. CPU is idle.")
             return
 
         self.memory.add_to_memory(page, self.using_CPU.pid)
@@ -152,7 +161,7 @@ class CPU:
         print ("")
         print("Using CPU:")
         if self.using_CPU != None:
-            print(self.using_CPU.pid)
+            print("PID", self.using_CPU.pid, "from level", self.using_CPU.level)
         else:
             print("[idle]")
 
@@ -164,39 +173,39 @@ class CPU:
             print("[empty]")
         else:
             for i in self.lvl_0_q:
-                print(i.pid)
+                print("PID", i.pid)
 
         print("Level 1: ")
         if len(self.lvl_1_q) == 0:
             print("[empty]")
         else:
             for i in self.lvl_1_q:
-                print(i.pid)
+                print("PID", i.pid)
 
         print("Level 2: ")
         if len(self.lvl_2_q) == 0:
             print("[empty]")
         else:
             for i in self.lvl_2_q:
-                print(i.pid)
-                
+                print("PID", i.pid)
+
         print ("")
 
     # "Shows what processes are currently using the hard disks and what processes are waiting to use them.
     # For each busy hard disk show the process that uses it and show its I/O-queue.
     # Make sure to display the filenames (from the d command) for each process. The enumeration of hard disks starts from 0."
     def show_disk(self):
+        print ("")
         for i in range(self.disks[0].hdd_count):
-            print ("")
             print( "Hard Disk {}:".format(i) )
             print( "Using disk:")
             if self.disks[i].using_HDD != None:
-                print(self.disks[i].using_HDD.pid, " : ", self.disks[i].using_HDD.file_name)
+                print("PID", self.disks[i].using_HDD.pid, "is using", "\"" + self.disks[i].using_HDD.file_name + "\"")
             else:
                 print( "[idle]" )
             print( "In I/O queue:")
             for j in self.disks[i].io_queue:
-                print(j.pid, " : ", j.file_name)
+                print("PID", j.pid, "wants to use",  "\"" + j.file_name + "\"")
             if len(self.disks[i].io_queue) == 0:
                 print ("[empty]")
             print ("")
